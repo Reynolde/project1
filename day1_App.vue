a30705ca1c83abf9bd416a0ca4ad582d2d45d8a1
Reynolde <baothong1038@gmail.com>
2024-11-09 15:50:40 +0700

    Add files via upload

---
+++ day1_App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import { ref } from "vue";
+
+const soLuong = ref(11);
+const Price = ref(12344)
+const name = ref("ghahahahah")
+
+</script>
+<template>
+    <div>
+        <h1>{{ soLuong }}</h1>
+        <h1>{{ name }}</h1>
+        <h1>{{ Price }}</h1>
+    </div>
+</template>
